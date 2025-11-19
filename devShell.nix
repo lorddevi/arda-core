27e06277cc69d119d42370aa807d7c6edcf3568f
Lord_Devi <lord@mgk.one>
2025-11-19 17:18:33 -0500

    feat: Add setuptools and wheel to devShell
    - Added pkgs.python313Packages.setuptools
    - Added pkgs.python313Packages.wheel
    - These are required for building the package from pyproject.toml

--- devShell.nix
+++ devShell.nix
@@ -11,13 +11,15 @@
           pkgs.gh
           pkgs.age
           pkgs.direnv
-	  pkgs.coreutils
-	  pkgs.which
+          pkgs.coreutils
+          pkgs.which
 
           # Include Python packages from overlaid python313Packages
           # This ensures rich-click 1.9.4 from our overlay is used
           pkgs.python313
           pkgs.python313Packages.pip
+          pkgs.python313Packages.setuptools
+          pkgs.python313Packages.wheel
           pkgs.python313Packages.click
           pkgs.python313Packages.pyyaml
           pkgs.python313Packages.rich
