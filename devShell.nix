a277fc688ea465d3167dd6cbed96a0140bf8286d
Lord_Devi <lord@mgk.one>
2025-11-19 13:38:29 -0500

    temp: Disable overlay to debug slow evaluation

--- devShell.nix
+++ devShell.nix
@@ -12,15 +12,14 @@
           pkgs.gh
           pkgs.age
 
-          # Include Python packages from overlaid python313Packages
-          # This ensures rich-click 1.9.4 from our overlay is used
-          pkgs.python313
-          pkgs.python313Packages.pip
-          pkgs.python313Packages.click
-          pkgs.python313Packages.pyyaml
-          pkgs.python313Packages.rich
-          pkgs.python313Packages.pydantic
-          pkgs.python313Packages.rich-click
+          # Include Python packages
+          pkgs.python3
+          pkgs.python3Packages.pip
+          pkgs.python3Packages.click
+          pkgs.python3Packages.pyyaml
+          pkgs.python3Packages.rich
+          pkgs.python3Packages.pydantic
+          pkgs.python3Packages.rich-click
         ];
       };
     };
