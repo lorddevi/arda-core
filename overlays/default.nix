dc8ba8c971fb7e73ac41fab1faeed942be78df14
Lord_Devi <lord@mgk.one>
2025-11-18 19:45:15 -0500

    fix: Correct overlay syntax for Python package overrides

--- overlays/default.nix
+++ overlays/default.nix
@@ -4,9 +4,8 @@
 final: prev:
 {
   # Override Python packages
-  python313Packages = prev.python313Packages.overrideScope' (selfOverlay:
-    { super, ... }:
-    {
+  python313Packages = prev.python313Packages.override {
+    overrides = self: super: {
       # Override rich-click to version 1.9.4 (has theming support)
       # Theming was added in 1.9.0, but Nixpkgs has 1.8.9
       rich-click = super.rich-click.overrideAttrs (old: {
@@ -19,6 +18,6 @@ final: prev:
           hash = "sha256-0000000000000000000000000000000000000000000000000000=";
         };
       });
-    }
-  );
+    };
+  };
 }
